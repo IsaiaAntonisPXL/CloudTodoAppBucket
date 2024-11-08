37f58731d4c97297085192052b3514081309c47b
IsaiaAntonisPXL <Isaia.antonis@student.pxl.be>
2024-11-08 16:52:36 +0100

    correctly receiving urls, now testing to display the images

--- frontend/src/app/components/carrousel/carrousel.component.ts
+++ frontend/src/app/components/carrousel/carrousel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CarrouselItem } from 'src/app/CarrouselItem.model';
 import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
@@ -8,30 +7,31 @@ import { TodoService } from 'src/app/services/todo.service';
   styleUrls: ['./carrousel.component.css']
 })
 export class CarrouselComponent implements OnInit {
-  images!: CarrouselItem[];
+  images: string[] = [];  // Array of image URLs
   activeItem: number = 0;
   counter!: any;
+
   constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
     this.todoService.getCarrouselItems().subscribe(
       (data: any) => {
-        this.images = data;
+        console.log('Carrousel Data:', data); // Log data to check response
+        this.images = data.images; // This should assign the image URLs array directly
       }
     );
 
-    this.counter = setInterval( () => {
-      this.activeItem = (this.activeItem >= this.images.length-1) ? 0 : this.activeItem+1;
-    },5000);
+    this.counter = setInterval(() => {
+      this.activeItem = (this.activeItem >= this.images.length - 1) ? 0 : this.activeItem + 1;
+    }, 5000); // Change image every 5 seconds
   }
 
-  ngOnDestroy(): void{
-    clearInterval(this.counter);
+  ngOnDestroy(): void {
+    clearInterval(this.counter); // Cleanup the interval
   }
 
   checkIfActive(index: number) {
-    return this.activeItem === index;
+    return this.activeItem === index; // Check if the current item is active
   }
-
 }
 
